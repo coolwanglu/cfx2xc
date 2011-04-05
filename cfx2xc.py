e788974323d9483969539ef5c371022163ce67e1
coolwanglu@gmail.com <coolwanglu@gmail.com@e103b20c-27c5-3ea9-49e1-ff78bd49be20>
2011-04-05 06:09:09 +0000

    supports script now

--- cfx2xc.py
+++ cfx2xc.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 """
-cfx2xc.py <http://code.google.com/p/cfx2xc/>
+cfx2xc.py
 
 Convert a CursorFX theme to an X11 Cursor theme
 
 by Wang Lu <coolwanglu(a)gmail.com>
 first version at 2011.04.04
-
-THIS FILE IS DISTRIBUTED UNDER GPLv3
 
 Based on the analysis from http://hi.baidu.com/fatfish888/blog/item/3592500392c27808738da516.html
 
@@ -22,11 +20,15 @@ import sys
 import logging
 import Image
 import os
+import re
 
 LOG_LEVEL=logging.DEBUG
 TMP_DIR='tmp_cfx2xc'
 ORIGINAL_DIR=TMP_DIR+'/original'
 OUTPUT_DIR=TMP_DIR+'/cursors'
+SCRIPT_DIR=TMP_DIR+'/scripts'
+CFG_DIR=TMP_DIR+'/cfgs'
+SCRIPT_LINE_PATTERN=re.compile(r'(\d+)(?:-(\d+))?(?:,(\d+))?')
 
 def try_mkdir(d):
     try:
@@ -42,6 +44,8 @@ class CursorXP():
         try_mkdir(TMP_DIR)
         try_mkdir(ORIGINAL_DIR)
         try_mkdir(OUTPUT_DIR)
+        try_mkdir(SCRIPT_DIR)
+        try_mkdir(CFG_DIR)
 
         data = open(fn).read()
         (self.version # maybe
@@ -116,21 +120,54 @@ size of script2: %u\n'\
             assert size_of_header_and_image == size_of_header_with_script + size_of_image
             assert size_of_image == image_width * image_height * 4
 
+            # crop images
             img = Image.fromstring("RGBA", (image_width, image_height), data[cur_pos+size_of_header_with_script:cur_pos+size_of_header_and_image], "raw", "BGRA", 0, -1)
             frame_width = image_width / frame_count
             frame_height = image_height
             for i in range(frame_count):
                 img.crop((frame_width*(i-1),0,frame_width*i,image_height)).save('%s/img%s_%d.png'%(ORIGINAL_DIR,image_index,i))
 
-            # need to parse script...
-            cfg = open('%s/img%d.cfg'%(TMP_DIR, image_index), 'w')
-            
+            # parse script...
+            # currently "repeat/end repeat" is not supported
+            cfg = open('%s/img%d.cfg'%(CFG_DIR, image_index), 'w')
             xcursor_size = max(frame_width, frame_height)
-            for i in range(frame_count):
-                cfg.write('%d %d %d %s/img%s_%d.png %d\n' % (xcursor_size, mouse_x, mouse_y, ORIGINAL_DIR, image_index, i, frame_interval))
+
+            if size_of_script > 0:
+                script_data = data[cur_pos+size_of_header_without_script:cur_pos+size_of_header_with_script].decode('utf-16le')[:-1].replace(';','\n').split()
+                
+                open('%s/script%d'%(SCRIPT_DIR, image_index),'w').write('\n'.join(script_data))
+
+                last_interval = frame_interval
+                for l in script_data:
+                    try:
+                        (start_frame, end_frame, interval) = SCRIPT_LINE_PATTERN.match(l).groups()
+                        start_frame = int(start_frame)
+                        if end_frame is None:
+                            end_frame = start_frame
+                        else:
+                            end_frame = int(end_frame)
+                        if interval is None:
+                            interval = last_interval
+                        else:
+                            interval = int(interval)
+                            last_interval = interval
+
+                        step = 1 if end_frame >= start_frame else -1
+
+                        # note that the frame index in the script is 1-based
+                        for i in range(start_frame, end_frame+step, step):
+                            cfg.write('%d %d %d %s/img%s_%d.png %d\n' % (xcursor_size, mouse_x, mouse_y, ORIGINAL_DIR, image_index, i-1, interval))
+
+                    except:
+                        logging.info('Cannot parse script line:\n%s' % (l,))
+                        raise
+                        pass
+            else:
+                for i in range(frame_count):
+                    cfg.write('%d %d %d %s/img%s_%d.png %d\n' % (xcursor_size, mouse_x, mouse_y, ORIGINAL_DIR, image_index, i, frame_interval))
             cfg.close() 
 
-            os.system('xcursorgen "%s/img%d.cfg" "%s/cur%d"' % (TMP_DIR, image_index, OUTPUT_DIR, image_index))
+            os.system('xcursorgen "%s/img%d.cfg" "%s/cur%d"' % (CFG_DIR, image_index, OUTPUT_DIR, image_index))
             
             cur_pos += size_of_header_and_image
          
